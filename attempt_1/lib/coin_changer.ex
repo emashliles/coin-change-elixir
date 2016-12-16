1eae84d7bfbea20b24daa8af869346a7ea12145f
EA <emashliles@gmail.com>
2016-12-16 14:34:45 +0000

    Can handle 1 and 2

--- attempt_1/lib/coin_changer.ex
+++ attempt_1/lib/coin_changer.ex
@@ -3,4 +3,10 @@ defmodule CoinChanger do
   def get_change(0) do
     0
   end
+
+  def get_change(amount) do
+    Enum.member?([1,2],amount )
+    amount
+  end
+
 end
--- attempt_1/test/attempt_1_test.exs
+++ attempt_1/test/attempt_1_test.exs
@@ -2,7 +2,15 @@ defmodule Attempt1Test do
   use ExUnit.Case
   doctest CoinChanger
 
-  test "returns 0 when the amount of change is 0" do
+  test "returns 0 when the amount given is 0" do
     assert CoinChanger.get_change(0) == 0
+  end
+
+  test "returns 1 when the amount given is 1" do
+    assert CoinChanger.get_change(1) == 1
+  end
+
+  test "returns 2 when the amount given is 2" do
+    assert CoinChanger.get_change(2) == 2
   end
 end
