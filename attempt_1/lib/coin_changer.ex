018051dcf8ac246f26cbf5063d636ecfa29073ac
EA <emashliles@gmail.com>
2016-12-19 16:08:26 +0000

    Handles 3 as an input

--- attempt_1/lib/coin_changer.ex
+++ attempt_1/lib/coin_changer.ex
@@ -4,12 +4,39 @@ defmodule CoinChanger do
     0
   end
 
-  def get_change(amount) when amount < 0 do
-    "Amount must be greater than zero"
+  def get_change(amount) when amount <= 0 do
+   "Amount must be greater than zero"
   end
 
   def get_change(amount) do
-    Enum.member?([1,2],amount )
-    amount
+    coins = [2,1]
+    find_coins(amount,[],coins)
+    end
+
+    def find_coins(amount,change,[]) do
+      change
+    end
+    
+    def find_coins(0, change, _) do
+      change
+    end
+   
+   def find_coins(1, [], _) do
+     [1]
+   end
+
+  def find_coins(amount,change,coins) do
+
+     [head | tail] = coins
+     rem = amount - head
+
+     if rem - head <0 do 
+       changen = change ++ [head]
+       find_coins(rem, changen, tail)
+     else
+       changenew = change ++ [head]
+       find_coins(rem, changenew, coins)
+     end
   end
+
 end
--- attempt_1/test/attempt_1_test.exs
+++ attempt_1/test/attempt_1_test.exs
@@ -7,11 +7,19 @@ defmodule Attempt1Test do
   end
 
   test "returns 1 when the amount given is 1" do
-    assert CoinChanger.get_change(1) == 1
+    assert CoinChanger.get_change(1) == [1]
   end
 
   test "returns 2 when the amount given is 2" do
-    assert CoinChanger.get_change(2) == 2
+    assert CoinChanger.get_change(2) == [2]
+  end
+
+  #test "returns 2 and 5 when the amount given is 7" do
+  #  assert CoinChanger.get_change(7) == [2,5]
+  #end
+
+  test "returns 1 and 2 when the amount given is 3" do
+    assert CoinChanger.get_change(3) == [2,1]
   end
 
   test "returns error if number is less than 0" do
